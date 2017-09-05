b2f41202e32722d46d5e5dee35178188b0c1f8e2
DragonBorn2333 <13591680321@163.com>
2017-09-05 09:30:13 +0800

    1

---
+++ src/com/cn/mahjong/entity/MahjongCards.java
@@ -0,0 +1,41 @@
+package com.cn.mahjong.entity;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * @author DargonBorn2333
+ *		麻将手牌实体类
+ *
+ */
+public class MahjongCards {
+	
+	//所有手牌组成的字符串
+	private String cardStr;
+	
+	//手牌详情     key :  100 条  200 饼  300 万  400 风   500 碰  600  吃  700 杠
+	private Map<Integer, List<Integer>> cardsDetail;
+
+	
+	
+	
+	public String getCardStr() {
+		return cardStr;
+	}
+
+	public void setCardStr(String cardStr) {
+		this.cardStr = cardStr;
+	}
+
+	public Map<Integer, List<Integer>> getCardsDetail() {
+		return cardsDetail;
+	}
+
+	public void setCardsDetail(Map<Integer, List<Integer>> cardsDetail) {
+		this.cardsDetail = cardsDetail;
+	}
+	
+	
+	
+}
---
+++ src/com/cn/mahjong/entity/Player.java
@@ -0,0 +1,55 @@
+package com.cn.mahjong.entity;
+
+/**
+ * 
+ * @author DragonBorn2333
+ *
+ *	玩家类
+ *
+ */
+public class Player {
+	
+	
+	public Player(){}
+	
+	public Player(Player nextPlayer,MahjongCards cards,boolean isZhuang){
+		this.nextPlayer = nextPlayer;
+		this.cards = cards;
+		this.isZhuang = isZhuang;
+	}
+	
+	//下家
+	private Player nextPlayer;
+	
+	//手牌
+	private MahjongCards cards;
+	
+	//是否是庄家
+	private boolean isZhuang;
+
+	public Player getNextPlayer() {
+		return nextPlayer;
+	}
+
+	public void setNextPlayer(Player nextPlayer) {
+		this.nextPlayer = nextPlayer;
+	}
+
+	public MahjongCards getCards() {
+		return cards;
+	}
+
+	public void setCards(MahjongCards cards) {
+		this.cards = cards;
+	}
+
+	public boolean isZhuang() {
+		return isZhuang;
+	}
+
+	public void setZhuang(boolean isZhuang) {
+		this.isZhuang = isZhuang;
+	}
+	
+	
+}
